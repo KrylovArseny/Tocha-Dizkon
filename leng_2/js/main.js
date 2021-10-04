0c1e3503177f92dd87f7278b18b45d9cf3d4bb08
KrylovArseny <arsenjfr@gmail.com>
2021-10-04 23:12:07 +0300

    1

--- leng_2/js/main.js
+++ leng_2/js/main.js
@@ -1,3 +1,13 @@
 window.onload = function() {
+  let x = document.getElementById("contain");
 
+  x.addEventListener("click", myFunction);
+
+  function myFunction() {
+      let element = document.getElementById("bottomMenu_ul");
+      element.classList.toggle("open");
+      let headerInner = document.getElementById("headerInner");
+      headerInner.classList.toggle("open");
+      x.classList.toggle("change");
+  }
 };
